7eeddd3613b6831c27890c58058f60be33bf322c
Igor de Souza Bezerra <igorbezerra01@outlook.com>
2022-09-07 15:30:09 -0300

    log

--- src/main/java/dev/igor/projetospring/ProjetoSpringApplication.java
+++ src/main/java/dev/igor/projetospring/ProjetoSpringApplication.java
@@ -1,13 +1,16 @@
 package dev.igor.projetospring;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjetoSpringApplication {
+    public static final Logger logger = LoggerFactory.getLogger(ProjetoSpringApplication.class);
 
     public static void main(String[] args) {
+        logger.info("Hello, World!");
         SpringApplication.run(ProjetoSpringApplication.class, args);
     }
-
 }
